80d5759dfb0888ce1f13c2e74d2840054d3b6583
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-14 19:54:28 +0300

    make test passing

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,23 +1,38 @@
 export class Board {
   width;
   height;
+  stationary;
   fallingBlock;
   fallingBlockRow = 0;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.stationary = this.emptyBoard(width, height);
+  }
+
+  emptyBoard(width, height) {
+    var board = Array(width).fill(null).map(() => Array(height).fill("."));
+    return board;
   }
 
   toString() {
     var s = "";
     for (let i = 0; i < this.width; i++) {
       for (let j = 0; j < this.height; j++) {
-        s += this.hasFallingAt(i, j) ? this.fallingBlock.getColor() : ".";
+        s += this.getColorAt(i, j);
       }
       s += "\n";
     }
     return s;
+  }
+
+  getColorAt(i, j) {
+    if (this.hasFallingAt(i, j)) {
+      return this.fallingBlock.getColor();
+    } else {
+      return this.stationary[i][j];
+    }
   }
 
   hasFallingAt(x, y) {
@@ -36,6 +51,11 @@ export class Board {
   }
 
   tick() {
-    this.fallingBlockRow++;
+    if (this.fallingBlockRow == this.height - 1) {
+      this.stationary[this.fallingBlockRow][1] = this.fallingBlock.getColor();
+      this.fallingBlock = null;
+    } else {
+      this.fallingBlockRow++;
+    }
   }
 }
