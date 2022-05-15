970d6c403edeaa09953c6beefec8aa94358f0850
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-15 13:06:09 +0300

    make test pass

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -7,7 +7,6 @@ export class RotatingShape {
         for (let i = 0; i < rows.length; i++) {
             this.blocks[i] = rows[i];
         }
-        console.log(this.blocks);
         Object.freeze(this);
     }
 
@@ -20,5 +19,16 @@ export class RotatingShape {
             s += "\n";
         }
         return s;
+    }
+
+    rotateRight() {
+        var rotatedBlocks = [];
+        for (let i = 0; i < this.blocks[0].length; i++) {
+            rotatedBlocks[i] = "";
+            for (let j = this.blocks.length - 1; j >= 0; j--) {
+                rotatedBlocks[i] += this.blocks[j][i];
+            }
+        }
+        return new RotatingShape(rotatedBlocks.join("\n"));
     }
 }
