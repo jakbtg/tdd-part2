d7f65914daf4c3942d30b7db19742cd804d56023
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-17 17:34:14 +0300

    create test rotate right with no space

--- test/RotatingFallingTetrominoes.test.mjs
+++ test/RotatingFallingTetrominoes.test.mjs
@@ -2,6 +2,22 @@ import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
 
+function moveToSide(board, left) {
+    for (let i = 0; i < 10; i++) {
+        if (left) {
+            board.moveLeft();
+        } else {
+            board.moveRight();
+        }
+    }
+}
+
+function fallToBottom(board) {
+    for (let i = 0; i < 10; i++) {
+        board.tick();
+    }
+}
+
 describe("Rotating falling tetrominoes", () => {
     let board;
     beforeEach(() => {
@@ -31,6 +47,41 @@ describe("Rotating falling tetrominoes", () => {
              ..........
              ..........
              ..........`
+        );
+    });
+
+});
+
+describe("Rotating falling tetrominoes when there is no space to rotate", () => {
+    let board;
+    beforeEach(() => {
+        board = new Board(10, 6);
+        board.drop(Tetromino.O_SHAPE);
+        moveToSide(board, true);
+        fallToBottom(board);
+        board.drop(Tetromino.O_SHAPE);
+        board.moveLeft();
+        board.moveLeft();
+        fallToBottom(board);
+        board.drop(Tetromino.O_SHAPE);
+        board.moveLeft();
+        board.moveLeft();
+        fallToBottom(board);
+
+        board.drop(Tetromino.I_SHAPE);
+        board.rotateRight();
+    }
+    );
+
+    it("cannot rotate right/clockwise when there is no space to rotate", () => {
+        board.rotateRight();
+        expect(board.toString()).to.equalShape(
+            `....I.....
+             ....I.....
+             ..OOI.....
+             ..OOI.....
+             OOOO......
+             OOOO......`
         );
     });
 
