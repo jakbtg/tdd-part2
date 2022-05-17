715e786551aec55a2821d8bf868eea2defc4ef59
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-17 11:08:11 +0300

    created first test

---
+++ test/MovingFallingTetrominoes.test.mjs
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Moving falling tetrominoes", () => {
+    let board;
+    beforeEach(() => {
+        board = new Board(10, 6);
+    }
+    );
+
+    xit("a falling tetromino can be moved left", () => {
+        board.drop(Tetromino.T_SHAPE);
+        board.moveLeft();
+        expect(board.toString()).to.equalShape(
+            `...T......
+             ..TTT.....
+             ..........
+             ..........
+             ..........
+             ..........`
+        );
+    });
+});
+
+
