57002ca9d8971d3b6286ca4bc30fbf95b5358182
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-18 13:05:59 +0300

    focus on FallingTetrominoes tests

--- test/FallingTetrominoes.test.mjs
+++ test/FallingTetrominoes.test.mjs
@@ -1,6 +1,7 @@
 
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
+import { RotatingShapeNew } from "../src/RotatingShapeNew.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
 
 function fallToBottom(board) {
@@ -11,12 +12,14 @@ function fallToBottom(board) {
 
 describe("Falling tetrominoes", () => {
   let board;
+  let shape = new RotatingShapeNew([`.T.\nTTT\n...\n`], 0);
   beforeEach(() => {
     board = new Board(10, 6);
+    board.drop(shape);
   });
 
   it("start from the top middle", () => {
-    board.drop(Tetromino.T_SHAPE);
+    // board.drop(shape);
 
     expect(board.toString()).to.equalShape(
       `....T.....
@@ -29,7 +32,7 @@ describe("Falling tetrominoes", () => {
   });
 
   it("stop when they hit the bottom", () => {
-    board.drop(Tetromino.T_SHAPE);
+    // board.drop(Tetromino.T_SHAPE);
     fallToBottom(board);
 
     expect(board.toString()).to.equalShape(
@@ -43,7 +46,7 @@ describe("Falling tetrominoes", () => {
   });
 
   it("stop when they land on another block", () => {
-    board.drop(Tetromino.T_SHAPE);
+    // board.drop(Tetromino.T_SHAPE);
     fallToBottom(board);
     board.drop(Tetromino.T_SHAPE);
     fallToBottom(board);
